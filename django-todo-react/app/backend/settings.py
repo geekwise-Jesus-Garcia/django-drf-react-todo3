8b86490b34c0c601020ea6f7e5c07391cc7f7bde
geekwise-Jesus-Garcia <jesus1287garcia@yahoo.com>
2019-12-03 22:28:16 +0000

    added serializers

--- django-todo-react/app/backend/settings.py
+++ django-todo-react/app/backend/settings.py
@@ -26,9 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
+    'corsheaders',            
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -107,3 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+         'localhost:3000/'
+     )
